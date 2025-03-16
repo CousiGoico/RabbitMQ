1e8e1ea7878fda6ae6c77e11340db7cff04456b6
CousiGoico <javier.cousino.goicolea@hotmail.com>
2025-03-16 13:30:15 +0100

    RabbitMQ

---
+++ NewTask/NewTask.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var message = GetMessage(args);
+        }
+
+        static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
---
+++ Worker/Worker.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+                arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received {message}");
+
+                int dots = message.Split('.').Length - 1;
+                await Task.Delay(dots * 1000);
+
+                Console.WriteLine(" [x] Done");
+            };
+
+            await channel.BasicConsumeAsync("hello", autoAck: true, consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
