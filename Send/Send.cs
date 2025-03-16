185b9ef92abaffa303cff1d2ab00d0f08cce2d9c
CousiGoico <javier.cousino.goicolea@hotmail.com>
2025-03-16 12:45:13 +0100

    RabbitMQ

---
+++ Send/Send.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMQ
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Call();
+        }
+
+        static async Task Call() {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+                arguments: null);
+
+            const string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
+            Console.WriteLine($" [x] Sent {message}");
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
